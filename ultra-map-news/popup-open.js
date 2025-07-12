9c5a00486fd1ff2d5aa17ff13a742eaa9a9d8b45
FRISK MAN <chiliuqingzuo@gmail.com>
2025-07-12 16:52:02 +0900

    Delete ultra-map-news directory

--- ultra-map-news/popup-open.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById('open-map').addEventListener('click', function() {
-    chrome.tabs.create({ url: chrome.runtime.getURL('map.html') });
-  });
-}); 
